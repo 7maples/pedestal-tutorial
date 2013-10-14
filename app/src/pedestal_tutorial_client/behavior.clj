6d01142aeff7e4d638f07ba90f6bbbc22abdd805
Jennifer Eliuk <jen7maples@gmail.com>
2013-10-14 14:24:44 -0600

    add button to increase counter

--- app/src/pedestal_tutorial_client/behavior.clj
+++ app/src/pedestal_tutorial_client/behavior.clj
@@ -3,6 +3,9 @@
               [io.pedestal.app.messages :as msg]
               [io.pedestal.app :as app]))
 
+(defn init-main [_]
+  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
 (defn inc-transform [old-value _]
   ((fnil inc 0) old-value))
 
@@ -10,12 +13,11 @@
   {:version 2
    :transform [[:inc [:my-counter] inc-transform]]
    :emit [{:init init-main}
-          {:in #{[:*]} :fn (app/default-emitter [:main]) :init init-main}]})
+          [#{[:*]} (app/default-emitter [:main])]]})
 
 (defn data-model [app]
   (-> app :state deref :data-model))
 
-(defn init-main [_]
-  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
 
 
