853633dedcdad02dad21c2513e26a2231f84d0fc
Jennifer Eliuk <jen7maples@gmail.com>
2013-10-14 13:51:05 -0600

    add emitter

--- app/src/pedestal_tutorial_client/behavior.clj
+++ app/src/pedestal_tutorial_client/behavior.clj
@@ -1,11 +1,21 @@
 (ns ^:shared pedestal-tutorial-client.behavior
     (:require [clojure.string :as string]
-              [io.pedestal.app.messages :as msg]))
+              [io.pedestal.app.messages :as msg]
+              [io.pedestal.app :as app]))
 
-(defn set-value-transform [old-value message]
-  (:value message))
+(defn inc-transform [old-value _]
+  ((fnil inc 0) old-value))
 
 (def example-app
   {:version 2
-   :transform [[:set-value [:greeting] set-value-transform]]})
+   :transform [[:inc [:my-counter] inc-transform]]
+   :emit [{:init init-main}
+          {:in #{[:*]} :fn (app/default-emitter [:main]) :init init-main}]})
+
+(defn data-model [app]
+  (-> app :state deref :data-model))
+
+(defn init-main [_]
+  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
 
