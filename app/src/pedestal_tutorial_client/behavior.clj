c35d50612c79c20ddf9b2280b10156771fb0d5b8
Jennifer Eliuk <jen7maples@gmail.com>
2013-10-25 14:11:56 -0600

    derived values working

--- app/src/pedestal_tutorial_client/behavior.clj
+++ app/src/pedestal_tutorial_client/behavior.clj
@@ -15,16 +15,38 @@
 (defn publish-counter [count]
   [{msg/type :swap msg/topic [:other-counters] :value count}])
 
+(defn total-count [_ nums] (apply + nums))
+
+(defn maximum [old-value nums]
+  (apply max (or old-value 0) nums))
+
+(defn average-count [_ {:keys [total nums]}]
+  (/ total (count nums)))
+
 (def example-app
   {:version 2
    :transform [[:inc  [:my-counter] inc-transform]
                [:swap [:**]         swap-transform]]
-   :effect #{[#{[:my-counter]} publish-counter :single-val]}
-   :emit [{:init init-main}
-          [#{[:my-counter] [:other-counters :*]} (app/default-emitter [:main])]]})
+   :effect    #{[#{[:my-counter]} publish-counter :single-val]}
+   :emit      [{:init init-main}
+                  [#{[:my-counter]
+                     [:other-counters :*]
+                     [:total-count]
+                     [:max-count]
+                     [:average-count]}
+                     (app/default-emitter [:main])]]
+   :derive    #{[#{[:my-counter] [:other-counters :*]} [:total-count] total-count :vals]
+                [#{[:my-counter] [:other-counters :*]} [:max-count] maximum :vals]
+
+                [{[:my-counter] :nums
+                  [:other-counters :*] :nums
+                  [:total-count] :total}
+                  [:average-count] average-count :map]}})
 
 (defn data-model [app]
   (-> app :state deref :data-model))
+
+; #{} octothorpe in front of curly braces creates literal set (w/o # would be map)
 
 
 
